ad428c33f6a3516679ca05475d4e6cd4b30dc844
Abhik Patra <theabhik2020@gmail.com>
2024-12-31 21:00:04 +0530

    refactor: switch from Stack to Tabs layout in RootLayout

--- basic_1/app/(home)/_layout.tsx
+++ basic_1/app/(home)/_layout.tsx
@@ -1,35 +1,38 @@
-import { Stack } from 'expo-router';
-import { Alert, Button, Text } from "react-native"
-export default function RootLayout() {
-  return (
-    <Stack
+import { Stack,Tabs } from 'expo-router';
+import { Alert, Button, Text } from "react-native";
 
-      screenOptions={{
-        // title:"Home",
-        // headerTintColor:"yellow",
-        // headerShown: false,
-        headerStyle: {
-          backgroundColor: 'white'
-        }
-      }}
-    >
-      <Stack.Screen name="index" options={{
-        title: "Home",
-        headerTintColor: "blue",
-        // headerShown: false,
-        headerStyle: {
-          backgroundColor: 'white'
-        },
-        headerRight: () => {
-          return (
-            <Button onPress={() => Alert.alert("Hi Bro !!")} title='He' />
-          )
-        }
-      }} />
-      <Stack.Screen name="explore" />
-      <Stack.Screen name="model" options={{
-        presentation:"modal"
-      }} />
-    </Stack>
-  )
-}
+export default Tabs;
+
+// export default function RootLayout() {
+//   return (
+//     <Stack
+
+//       screenOptions={{
+//         // title:"Home",
+//         // headerTintColor:"yellow",
+//         // headerShown: false,
+//         headerStyle: {
+//           backgroundColor: 'white'
+//         }
+//       }}
+//     >
+//       <Stack.Screen name="index" options={{
+//         title: "Home",
+//         headerTintColor: "blue",
+//         // headerShown: false,
+//         headerStyle: {
+//           backgroundColor: 'white'
+//         },
+//         headerRight: () => {
+//           return (
+//             <Button onPress={() => Alert.alert("Hi Bro !!")} title='He' />
+//           )
+//         }
+//       }} />
+//       <Stack.Screen name="explore" />
+//       <Stack.Screen name="model" options={{
+//         presentation:"modal"
+//       }} />
+//     </Stack>
+//   )
+// }
